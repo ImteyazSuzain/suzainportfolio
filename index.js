51e668bf50f6f1dc35370922da946f86dea6f65c
ImteyazSuzain <suzainimteyaz@gmail.com>
2023-01-25 00:25:13 +0530

    hope so

--- index.js
+++ index.js
@@ -21,12 +21,12 @@ app.post("/contact", async (req, res) => {
 		success: "Email sent to Suzain Imteyaz",
 	});
 });
-/*if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === "production") {
 	app.use(express.static("client/build"));
 	app.get("*", (req, res) =>
 		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
 	);
-}*/
+}
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, console.log(`server listing to port 5000 only`));
