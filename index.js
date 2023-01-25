179c719649a1a10fa83e63d2ecdda1931628b0ab
ImteyazSuzain <suzainimteyaz@gmail.com>
2023-01-25 10:45:58 +0530

    hope so

--- index.js
+++ index.js
@@ -22,11 +22,9 @@ app.post("/contact", async (req, res) => {
 	});
 });
 if (process.env.NODE_ENV === "production") {
-	
-	app.get("*", (req, res) =>{
-		console.log(`connected`);
-	}
-		
+	app.use(express.static("client/build"));
+	app.get("*", (req, res) =>
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
 	);
 }
 
